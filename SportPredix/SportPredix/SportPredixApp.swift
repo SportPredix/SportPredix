8643dd3541730dc4721ccfa4d05b35e7df89c2fe
enribocco <142581742+enribocco@users.noreply.github.com>
2026-01-12 18:59:31 +0100

    aggiunta file base

---
+++ SportPredix/SportPredix/SportPredixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportPredixApp.swift
+//  SportPredix
+//
+//  Created by Francesco on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SportPredixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
